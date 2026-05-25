8eaa6d872e625f6f8a61f31a97f2610157fee791
Kenneth <james.kenneth503@gmail.com>
2026-05-24 21:49:04 -0600

    Add files via upload

---
+++ Azure/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi proyecto de Azure</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h1>Hola, este es mi proyecto de Microsoft Azure x Github</h1>
+
+<script src="js/script.js"></script>
+</body>
+</html>
